5a04a367d2cfd13265deeae166af1b89a74bf2cc
marcosun <marcoinbristol@gmail.com>
2019-07-21 16:57:20 +0800

    [Text] Introducing a new module Text, which can render hundreds of thousands of texts on map.

---
+++ src/Text/index.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text as AMapCanvasText } from 'amap-2drender';
+
+class Text extends React.PureComponent {
+  componentDidMount() {
+    this.text = new AMapCanvasText({
+      /**
+       * Canvas default size is equal to map size.
+       */
+      ...this.props.map.getSize(),
+      ...this.props,
+    });
+  }
+
+  componentDidUpdate() {
+    this.text.render({
+      /**
+       * Canvas default size is equal to map size.
+       */
+      ...this.props.map.getSize(),
+      ...this.props,
+    });
+  }
+
+  componentWillUnmount() {
+    this.text.destroy();
+  }
+
+  render() {
+    return null;
+  }
+}
+
+/**
+ * All properties are used to initialise AMapCanvasText instance through
+ * a single this.props variable.
+ */
+/* eslint-disable react/no-unused-prop-types */
+Text.propTypes = {
+  /**
+   * A list of texts.
+   */
+  data: PropTypes.arrayOf(PropTypes.shape({
+    /**
+     * Text colour.
+     * Default black.
+     */
+    color: PropTypes.string,
+    /**
+     * Font size in unit pixel.
+     * Default 10.
+     */
+    fontSize: PropTypes.number,
+    /**
+     * Text lng lat location. i.e. [lng, lat].
+     * Text position is derived from location.
+     * It has lower priority if both location and position are defined.
+     * Text anchor origin point is placed to this location.
+     */
+    location: PropTypes.arrayOf(PropTypes.number),
+    /**
+     * Text position in canvas cartesian coordinate system. i.e. [x, y].
+     * It has higher priority if both location and position are defined.
+     * Text anchor origin point is placed to this position.
+     */
+    position: PropTypes.arrayOf(PropTypes.number),
+    /**
+     * Text content.
+     * Default ''.
+     */
+    text: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  })),
+  /**
+   * Canvas height.
+   * Default 0.
+   */
+  height: PropTypes.number,
+  /**
+   * AMap instance.
+   */
+  map: PropTypes.object.isRequired,
+  /**
+   * Callback fired when at least a text is clicked.
+   * Signature:
+   * (event, texts) => void
+   * event: AMap MapsEvent object.
+   * texts: A list of texts that is clicked. Texts with the earlier position in the data array
+   * are positioned later in the click callback. This is because texts appear later in the data
+   * array are drawn later and has a higher priority when clicked.
+   */
+  onClick: PropTypes.func,
+  /**
+   * Callback fired when pointer leaves the element or one of its child elements (even if
+   * the pointer is still within the element).
+   * Signature:
+   * (event, texts) => void
+   * event: AMap MapsEvent object.
+   * texts: A list of texts that pointer overs. Texts with the earlier position in the data
+   * array are positioned later in the mouse over callback. This is because texts appear later
+   * in the data array are drawn later and has a higher priority when mouse over.
+   */
+  onMouseOut: PropTypes.func,
+  /**
+   * Callback fired when pointer moves onto the element or one of its child elements (even if
+   * the pointer is still within the element).
+   * Signature:
+   * (event, texts) => void
+   * event: AMap MapsEvent object.
+   * texts: A list of texts that pointer overs. Texts with the earlier position in the data
+   * array are positioned later in the mouse over callback. This is because texts appear later
+   * in the data array are drawn later and has a higher priority when mouse over.
+   */
+  onMouseOver: PropTypes.func,
+  /**
+   * Custom layer opacity.
+   * Default 1.
+   */
+  opacity: PropTypes.number,
+  /**
+   * Canvas height.
+   * Default 0.
+   */
+  width: PropTypes.number,
+  /**
+   * Custom layer zIndex.
+   * Default 12.
+   */
+  zIndex: PropTypes.number,
+  /**
+   * Custom layer visible zoom ranges.
+   * Default [3, 18]
+   */
+  zooms: PropTypes.arrayOf(PropTypes.number),
+};
+
+Text.defaultProps = {
+  data: [],
+};
+
+export default Text;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Grid } from './Grid';
 export { default as Line } from './Line';
 export { default as Marker } from './Marker';
+export { default as Text } from './Text';
